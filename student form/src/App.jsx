d9b12826271d49dcf5f5ceb0433a3c88aafba825
kaviya <kaviyapanneer06@gmail.com>
2025-09-05 18:46:10 +0530

    initial commit

---
+++ student form/src/App.jsx
@@ -0,0 +1,15 @@
+// App.jsx
+import React from "react";
+import StudentForm from "./StudentForm";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1>Student Registration form</h1>
+      <StudentForm />
+    </div>
+  );
+}
+
+export default App;
